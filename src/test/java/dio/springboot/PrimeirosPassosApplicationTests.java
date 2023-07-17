f9a77cd14e4df575a42c7e95811570d505e2d49d
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-17 16:24:02 -0300

    Aula - Primeiros passos

--- src/test/java/dio/springboot/PrimeirosPassosApplicationTests.java
+++ src/test/java/dio/springboot/PrimeirosPassosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PrimeirosPassosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
